7f974212bbe13366fa9826c2e6bb0739e1c38dd3
Mealman1551 <nathandubuy4@gmail.com>
2026-05-16 00:24:38 +0200

    Update adc.py

--- adc.py
+++ adc.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-# ADC Archiver - Launcher
-# This code is licensed under the GNU General Public License v3.0.
 
 import sys
 import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 
-if __name__ == "__main__":
-    from src.adc.__main__ import run
+from adc.__main__ import run
 
+if __name__ == "__main__":
     run()
